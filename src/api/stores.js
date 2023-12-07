27a0d7634d52edd83c6039d41466986d50eafd59
Hyeo Min Park <hyeominpark@Hyeos-MacBook-Air.local>
2023-12-07 17:08:03 +0900

    axios+tanstack package added

---
+++ src/api/stores.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+export const fetchStores = async () => {
+  try {
+    const response = await axios.get(`${process.env.REACT_APP_STORE_SERVER_URL}/stores`);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+export const deleteStore = async (storeId) => {
+  try {
+    await axios.delete(`http://localhost:4000/stores`, storeId);
+    return;
+  } catch (error) {
+    return console.error('Error deleting data:', error);
+  }
+};
+
+export const addStore = async (newStore) => {
+  try {
+    await axios.post(`http://localhost:4000/stores`, newStore);
+  } catch (error) {
+    return console.error('Error posting store', error);
+  }
+};
--- src/components/EditorsPick.jsx
+++ src/components/EditorsPick.jsx
@@ -4,23 +4,12 @@ import Background2 from '../assets/Background2.png';
 import axios from 'axios';
 import theme from '../styles/theme';
 import { useNavigate } from 'react-router-dom';
+import { fetchStores } from '../api/stores';
 
 function EditorsPick({ listRef }) {
   const [storeData, setStoreData] = useState([]);
 
   const navigate = useNavigate();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://localhost:4000/stores');
-        setStoreData(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-    fetchData();
-  }, []);
 
   return (
     <Container ref={listRef}>
